0bc455657b1eb2ca1e70af6982087060b0f233e3
arilybaert <arilybae@arteveldehs.be>
2020-06-02 18:30:04 +0200

    minor changes

--- resources/views/admin/about/edit.blade.php
+++ resources/views/admin/about/edit.blade.php
@@ -9,12 +9,12 @@
 
                 <div class="form-row">
                     <div class="form-group col-6">
-                        <label for="title_left_nl" class="text-white">title_left_nl</label>
+                        <label for="title_left_nl" class="text-white">Title Left NL</label>
                         <input type="text" class=" form-control @error('title_left_nl') invalid @enderror" id="title_left_nl" name="title_left_nl" value="{{ old('title_left_nl', ($about ? $about->title_left_nl : '')) }}">
 
                       </div>
                       <div class="form-group col-6">
-                          <label for="title_left_en" class="text-white">title_left_en</label>
+                          <label for="title_left_en" class="text-white">Title Left NL</label>
                           <input type="text" class="form-control @error('title_left_en') invalid @enderror" id="title_left_en" name="title_left_en" value="{{ old('title_left_en', ($about ? $about->title_left_en : '')) }}" class="">
                         </div>
                 </div>
@@ -22,12 +22,12 @@
 
                 <div class="form-row">
                     <div class="form-group col-6">
-                        <label for="content_left_nl" class="text-white">content_left_nl</label>
+                        <label for="content_left_nl" class="text-white">Content Left NL</label>
                         <textarea class="form-control @error('content_left_nl') invalid @enderror" id="content_left_nl" rows="9" name="content_left_nl" value="{{ old('content_left_nl', ($about ? $about->content_left_nl : '')) }}">{{ old('content_left_nl', ($about ? $about->content_left_nl : '')) }}</textarea>
                       </div>
 
                       <div class="form-group col-6">
-                        <label for="content_left_en" class="text-white">content_left_en</label>
+                        <label for="content_left_en" class="text-white">Content Left NL</label>
                         <textarea class="form-control @error('content_left_en') invalid @enderror" id="content_left_en" rows="9" name="content_left_en" value="{{ old('content_left_en', ($about ? $about->content_left_en : '')) }}">{{ old('content_left_en', ($about ? $about->content_left_en : '')) }}</textarea>
                       </div>
 
